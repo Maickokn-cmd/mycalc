90b688b774312ed295e4ddad9716d83e64fa6299
maickon <maickon0729@gmail.com>
2025-04-29 20:45:15 -0300

    adicionando alteração de código

---
+++ code.py
@@ -0,0 +1,7 @@
+# MyCalc
+
+Print("uma calculadora simples de operações matemáticas básicas: soma, subtração, multiplicação e divisão.
+
+Cada operação foi desenvolvida em uma branch separada, e o código final unificado está na branch main.")
+
+
