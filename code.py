06f8e1502b499021c3c0d7aa1d467fa81d7f35a2
thievery001 <bielfelipe9842@gmail.com>
2025-04-29 20:38:03 -0300

    atualizando code.py

--- code.py
+++ code.py
@@ -4,6 +4,16 @@ print("Uma calculadora simples de operações matemáticas básicas: soma, subtr
 
 Cada operação foi desenvolvida em uma branch separada, e o código final unificado está na branch main")
 
+#Soma, ela adiciona valores. É representada pelo símbolo (+)
 
+a = float(input("Digite o primeiro número: "))
+b = float(input("Digite o segundo número: "))
+print(f"Resultado da soma: {a + b}"
+
+#Exemplo:
+#a = 7
+#b = 5
+# soma = a + b
+#print(f"A soma de {a} e {b} é {soma}")
 
 
